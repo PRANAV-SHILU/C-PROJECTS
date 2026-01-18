6ff0d3d1b07408f04946db803097b182cad9c3f6
PRANAV SHILU <pranavshilu1811@gmail.com>
2026-01-18 14:18:06 +0530

    Project 8 - Tic-Tac-Toe

--- 8 Tic-Tac-Toe/tic-tac-toe.c
+++ 8 Tic-Tac-Toe/tic-tac-toe.c
@@ -99,6 +99,26 @@ int is_valid_move(char board[BOARD_SIZE][BOARD_SIZE], int row, int col)
 
 void player_move(char board[BOARD_SIZE][BOARD_SIZE])
 {
+  int count = 0, x, y;
+  for (int i = 0; i < BOARD_SIZE; i++)
+  {
+    for (int j = 0; j < BOARD_SIZE; j++)
+    {
+      if (board[i][j] == ' ')
+      {
+        count++;
+        x = i;
+        y = j;
+      }
+    }
+  }
+
+  if (count == 1)
+  {
+    board[x][y] = X;
+    return;
+  }
+
   int row, col;
   do
   {
